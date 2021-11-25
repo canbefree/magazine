a3e144c2b5d4f76b06d2c75b7c3c370ac45eae8c
canbefree <452198757@qq.com>
2021-11-25 15:23:31 +0800

    save~

--- utils/error.go
+++ utils/error.go
@@ -1,9 +1,17 @@
 package utils
 
 import (
+	"fmt"
+
 	jww "github.com/spf13/jwalterweatherman"
 )
 
 func FatalIfError(err error) {
 	jww.ERROR.Fatalf(err.Error())
+}
+
+func PanicIfErr(err error) {
+	if err != nil {
+		panic(fmt.Sprintf("err:%v", err.Error()))
+	}
 }
